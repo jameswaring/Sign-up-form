577c80d511a6ca6c6e782fc8d1182383277bd062
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-05-29 21:10:16 -0400

    validaiton complete but need fix on password non match

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector("#form-section")
-const password = document.querySelector("#password").value
-const passwordconf = document.querySelector("#passwordConf").value
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    console.log(password)
-    console.log(passwordconf)
-    if(password != passwordconf){
+    // check whether the passwords are the same
+    if(!passConfirmCheck){
         document.getElementById("passmessage").innerHTML = "Password's don't match"
     }
+    // check whether the password is strong enough
+    else if(!checkPassStrength){
+        document.getElementById("passmessage").innerHTML = "Use letters, numbers, upper and lower case"
+    }
+    // success message
+    else{
+        document.getElementById("passmessage").innerHTML = "Account created. Redirecting..."
+    }
 })
+
+
+function passConfirmCheck(){
+    const password = document.querySelector("#password").value
+    const passwordconf = document.querySelector("#passwordConf").value
+    return (password===passwordconf ? true : false)
+}
+
+function checkPassStrength(){
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+    const password = document.querySelector("#password").value
+    return (regex.test(password) ? true : false)
+}
